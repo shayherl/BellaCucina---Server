3768351a8740af3e6a3c3b558d54c72e32d49f9a
NaorBenEvgi <naorbenevgi@gmail.com>
2020-06-03 21:34:47 +0300

    first commit based on lab9

---
+++ sql scripts/CREATE TABLE [dbo].[users](.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[users](
+	[user_id] [UNIQUEIDENTIFIER] PRIMARY KEY NOT NULL default NEWID(),
+	[username] [varchar](30) NOT NULL UNIQUE,
+	[password] [varchar](300) NOT NULL
+)
+
