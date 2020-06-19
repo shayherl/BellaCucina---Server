d6bf482c84e163e7618c61c238de4294411575f5
NaorBenEvgi <naorbenevgi@gmail.com>
2020-06-19 22:08:47 +0300

    defult session time: 24 hours

--- main.js
+++ main.js
@@ -14,8 +14,8 @@ app.use(
   session({
     cookieName: "session", // the cookie key name
     secret: process.env.COOKIE_SECRET, // the encryption key
-    duration: 20 * 60 * 1000, // expired after 20 sec
-    activeDuration: 0 // if expiresIn < activeDuration,
+    // duration: 20 * 60 * 1000, // expired after 20 sec
+    // activeDuration: 0 // if expiresIn < activeDuration,
     //the session will be extended by activeDuration milliseconds
   })
 );
