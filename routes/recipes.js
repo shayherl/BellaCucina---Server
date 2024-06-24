8017b7fa013a222c3da0f5f9938d4512bc205634
AmitKravchik <74196105+AmitKravchik@users.noreply.github.com>
2024-06-24 19:55:57 +0300

    Update recipes.js

--- routes/recipes.js
+++ routes/recipes.js
@@ -4,6 +4,21 @@ const recipes_utils = require("./utils/recipes_utils");
 
 router.get("/", (req, res) => res.send("im here"));
 
+/**
+ * This path is for searching a recipe
+ */
+router.get("/search", async (req, res, next) => {
+  try {
+    const recipeName = req.query.recipeName;
+    const cuisine = req.query.cuisine;
+    const diet = req.query.diet;
+    const intolerance = req.query.intolerance;
+    const number = req.query.number || 5;
+    const results = await recipes_utils.searchRecipe(recipeName, cuisine, diet, intolerance, number);
+    res.send(results);
+  } catch (error) {
+    next(error);
+  }
 
 /**
  * This path returns a full details of a recipe by its id
